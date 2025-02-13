dc9c3ebfd25dddd87a4b77cefaa8d05d9c599a56
bq-nikita-58 <nikita.rani@betaque.com>
2025-02-13 20:08:05 +0530

    Update 2. variables.js

--- Javascript basics/2. variables.js
+++ Javascript basics/2. variables.js
@@ -67,3 +67,91 @@ arr.num1 = 30;        //allowed
 console.log(arr.num1);
 
 
+//-------------------------------------------------------------------
+
+/*
+Difference between var and let:-
+Let :- 
+-let work at block scope level (its access is limited to the block in which it is declared)
+-Prevents accidental variable access: If you want to keep a variable strictly within a specific block (like within a loop or conditional), let ensures it isn't accessible outside.
+
+case 1:-
+{
+    let b = 30;
+}
+console.log(b); // error: b is not defined
+
+case 2:-
+{
+    let c = 1;
+    {
+        let c = 10
+        console.log(c);   // 10
+    }
+    console.log(c);    // 1
+}
+
+case 3:-
+let a =10;
+function f(){
+    let a = 20;
+    console.log(a); //20
+    {
+        let a=100;
+        console.log(a); //100
+    }
+    console.log(a); //20
+}
+f();
+console.log(a); //10
+
+Example:-
+for(let a = 2 ; a>0; a--){
+    for(let a = 5 ; a>0; a--){
+        console.log(a);  // 5 > 4 > 3 > 2 > 1 > 5 > 4 > 3 > 2 > 1
+    }
+}
+
+
+VAR:-
+-var work at global level (Var can be declared and accessed globally)
+-var makes the variable available throughout the entire function, which can sometimes create issues if you're not expecting the variable to leak outside a block.
+
+case 1:-
+{
+    var a = 20;
+}
+console.log(a); // output 20
+
+case 2:-
+{
+    var c = 1;
+    {
+        var c = 10
+        console.log(c);   // 10
+    }
+    console.log(c);    // 10
+}
+
+case 3:-
+var a =10;
+function f(){
+    var a = 20;
+    console.log(a);   //20
+    {
+        var a=100;
+        console.log(a);   // 100
+    }
+    console.log(a);   //100
+}
+f();
+console.log(a); //10
+
+Example :
+for(var a = 2 ; a>0; a--){
+    for(var a = 5 ; a>0; a--){
+        console.log(a);   // 5 > 4 > 3 > 2 > 1
+    }
+}
+
+*/
